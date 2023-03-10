a15c54c4dd3b7f7cdbe9ecbba4644e82621d42c5
Jinwoo Kim <kidjinwoo@me.com>
2023-03-11 07:17:39 +0900

    no message

---
+++ Publisher/PublisherXPCService/ServiceDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ServiceDelegate.h
+//  CoreDataXPC
+//
+//  Created by Jinwoo Kim on 3/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ServiceDelegate : NSObject <NSXPCListenerDelegate>
+@end
+
+NS_ASSUME_NONNULL_END
--- Publisher/PublisherXPCService/main.m
+++ Publisher/PublisherXPCService/main.m
@@ -6,32 +6,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "PublisherXPCService.h"
-
-@interface ServiceDelegate : NSObject <NSXPCListenerDelegate>
-@end
-
-@implementation ServiceDelegate
-
-- (BOOL)listener:(NSXPCListener *)listener shouldAcceptNewConnection:(NSXPCConnection *)newConnection {
-    NSXPCInterface *exportedInterface = [NSXPCInterface interfaceWithProtocol:@protocol(PublisherXPCServiceProtocol)];
-    [exportedInterface setClasses:[NSSet setWithArray:@[NSDictionary.class, NSArray.class, NSString.class, NSURL.class]]
-                      forSelector:@selector(objectsDidChange:withReply:)
-                    argumentIndex:0
-                          ofReply:NO];
-    
-    newConnection.exportedInterface = exportedInterface;
-    
-    PublisherXPCService *exportedObject = [PublisherXPCService new];
-    newConnection.exportedObject = exportedObject;
-    [exportedObject release];
-    
-    [newConnection activate];
-    
-    return YES;
-}
-
-@end
+#import "ServiceDelegate.h"
 
 int main(int argc, const char *argv[]) {
     ServiceDelegate *delegate = [ServiceDelegate new];
